d0384f8cbb2b15e1b9555eeaef5edadfd8e8482a
Mert <33592577+MrPotatoLord@users.noreply.github.com>
2023-12-01 10:18:59 +0200

    Update doit.sh

--- firefoxdev/doit.sh
+++ firefoxdev/doit.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo cp -rp firefox_dev.desktop ~/.local/share/applications/
 wget -O firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"
 sudo cp -rp firefox*.tar.bz2 /opt
 sudo rm -rf firefox*.tar.bz2
-cd /opt
-sudo tar xjf firefox*.tar.bz2
-sudo rm -rf firefox*.tar.bz2
+sudo tar xjf /opt/firefox*.tar.bz2
+sudo rm -rf /opt/firefox*.tar.bz2
 sudo chown -R $USER /opt/firefox
+sudo cp -rp firefox_dev.desktop ~/.local/share/applications/
 
 #if line 6 doesnt work  tar -x -f firefox.tar.bz2
 #chmod +x doit.sh
