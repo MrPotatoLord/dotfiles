0a732613f7f7d8ac12d69025f0b108405adcc850
Mert <33592577+MrPotatoLord@users.noreply.github.com>
2025-12-15 17:25:07 +0100

    Add 'roslyn_ls' and 'clangd' to LSP servers

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "somesass_ls" }
+local servers = { "html", "cssls", "ts_ls", "somesass_ls", "roslyn_ls", "clangd" }
 vim.lsp.config("ts_ls", {
   init_options = {
     preferences = {
