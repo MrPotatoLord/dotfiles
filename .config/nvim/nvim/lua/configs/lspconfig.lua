674c7fbeb2f322629ef37a0138696a3e1c6023e9
KindaTired <mertcansahinileri@gmail.com>
2026-02-05 15:46:53 +0100

    new nvim config

---
+++ .config/nvim/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,22 @@
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = { "html", "cssls", "ts_ls", "somesass_ls", "roslyn_ls", "clangd" }
+vim.lsp.config("ts_ls", {
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    },
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "html",
+  },
+})
+vim.lsp.enable(servers)
+
+-- read :h vim.lsp.config for changing options of lsp servers
